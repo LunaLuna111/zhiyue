728ea15d7b5bc37e86429a2f1911f1311a21afd4
Zhiyue Project <opensource@users.noreply.github.com>
2026-09-21 04:00:36 +0800

    启用 Android Impeller 玻璃渲染

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -6,8 +6,8 @@
 /// version declared below.
 library;
 
-const zhiyueVersionName = '0.5.1';
-const zhiyueAndroidVersionCode = 5001;
+const zhiyueVersionName = '0.5.2';
+const zhiyueAndroidVersionCode = 5002;
 
 int androidVersionCodeFor(String versionName) {
   final match = RegExp(r'^(\d+)\.(\d+)\.(\d+)$').firstMatch(versionName);

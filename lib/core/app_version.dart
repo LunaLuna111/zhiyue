6caaa8f1d5014600dd733f5250d6d0333e887724
Zhiyue Project <opensource@users.noreply.github.com>
2026-09-22 20:59:15 +0800

    升级客户端至 0.5.9

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -6,8 +6,8 @@
 /// version declared below.
 library;
 
-const zhiyueVersionName = '0.5.8';
-const zhiyueAndroidVersionCode = 5008;
+const zhiyueVersionName = '0.5.9';
+const zhiyueAndroidVersionCode = 5009;
 
 int androidVersionCodeFor(String versionName) {
   final match = RegExp(r'^(\d+)\.(\d+)\.(\d+)$').firstMatch(versionName);

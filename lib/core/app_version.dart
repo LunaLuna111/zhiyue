6ce84a4de4514e4ef3605ae94381a557c93264c8
Zhiyue Project <opensource@users.noreply.github.com>
2026-09-24 14:36:18 +0800

    发布知阅 0.6.2

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -6,8 +6,8 @@
 /// version declared below.
 library;
 
-const zhiyueVersionName = '0.6.0';
-const zhiyueAndroidVersionCode = 6000;
+const zhiyueVersionName = '0.6.2';
+const zhiyueAndroidVersionCode = 6002;
 
 int androidVersionCodeFor(String versionName) {
   final match = RegExp(r'^(\d+)\.(\d+)\.(\d+)$').firstMatch(versionName);
